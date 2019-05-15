1f01f354eb326af800afac5b7d0473c56b33ea6e
Bram Gerritsen <bgerritsen@emico.nl>
2019-05-15 17:26:44 +0200

    Implement option to toggle hiding selected filters

---
+++ src/Model/FilterManager.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @author Bram Gerritsen <bgerritsen@emico.nl>
+ * @copyright (c) Emico B.V. 2017
+ */
+
+namespace Emico\AttributeLandingTweakwise\Model;
+
+use Emico\AttributeLanding\Api\Data\LandingPageInterface;
+use Emico\AttributeLanding\Model\FilterHider\FilterHiderInterface;
+use Emico\AttributeLanding\Model\LandingPageContext;
+use Emico\Tweakwise\Model\Catalog\Layer\Filter\Item;
+use Magento\Catalog\Model\Layer;
+use Magento\Catalog\Model\Layer\Resolver;
+
+class FilterManager
+{
+    /**
+     * @var array
+     */
+    private $activeFilters;
+
+    /**
+     * @var array
+     */
+    private $activeFiltersExludingLandingPageFilters;
+
+    /**
+     * @var Resolver
+     */
+    private $layerResolver;
+
+    /**
+     * @var FilterHiderInterface
+     */
+    private $filterHider;
+
+    /**
+     * @var LandingPageContext
+     */
+    private $landingPageContext;
+
+    /**
+     * FilterManager constructor.
+     * @param Resolver $layerResolver
+     * @param FilterHiderInterface $filterHider
+     */
+    public function __construct(Resolver $layerResolver, FilterHiderInterface $filterHider, LandingPageContext $landingPageContext)
+    {
+        $this->layerResolver = $layerResolver;
+        $this->filterHider = $filterHider;
+        $this->landingPageContext = $landingPageContext;
+    }
+
+    /**
+     * @return array
+     */
+    public function getActiveFiltersExcludingLandingPageFilters(): array
+    {
+        if ($this->activeFiltersExludingLandingPageFilters === null) {
+            $filters = $this->getAllActiveFilters();
+            $landingPage = $this->landingPageContext->getLandingPage();
+            if ($landingPage === null) {
+                return $filters;
+            }
+
+            foreach ($filters as $index => $filterItem) {
+                /** @var Item $filterItem */
+                if ($this->filterHider->shouldHideFilter($landingPage, $filterItem->getFilter(), $filterItem)) {
+                    unset($filters[$index]);
+                }
+            }
+            $this->activeFiltersExludingLandingPageFilters = $filters;
+        }
+        return $this->activeFiltersExludingLandingPageFilters;
+    }
+
+    /**
+     * @return array|Item[]
+     */
+    public function getAllActiveFilters(): array
+    {
+        if ($this->activeFilters !== null) {
+            return $this->activeFilters;
+        }
+
+        $filters = $this->getLayer()->getState()->getFilters();
+        if (!\is_array($filters)) {
+            return [];
+        }
+        $this->activeFilters = $filters;
+        return $this->activeFilters;
+    }
+
+    /**
+     * @return Layer
+     */
+    protected function getLayer(): Layer
+    {
+        return $this->layerResolver->get();
+    }
+
+    /**
+     * @param LandingPageInterface $landingPage
+     * @param Item $filterItem
+     * @return bool
+     */
+    public function isFilterAvailableOnLandingPage(LandingPageInterface $landingPage, Item $filterItem): bool
+    {
+        foreach ($landingPage->getFilters() as $landingPageFilter) {
+            if (
+                $filterItem->getAttribute()->getTitle() === $landingPageFilter->getValue() &&
+                $filterItem->getFilter()->getUrlKey() === $landingPageFilter->getFacet()
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/Plugin/UrlPlugin.php
+++ src/Plugin/UrlPlugin.php
@@ -8,9 +8,13 @@ namespace Emico\AttributeLandingTweakwise\Plugin;
 
 
 use Closure;
+use Emico\AttributeLanding\Api\Data\LandingPageInterface;
+use Emico\AttributeLanding\Model\LandingPageContext;
 use Emico\AttributeLanding\Model\UrlFinder;
+use Emico\AttributeLandingTweakwise\Model\FilterManager;
 use Emico\Tweakwise\Model\Catalog\Layer\Filter\Item;
 use Emico\Tweakwise\Model\Catalog\Layer\Url;
+use Magento\Catalog\Model\Layer;
 use Magento\Catalog\Model\Layer\Resolver;
 
 class UrlPlugin
@@ -24,16 +28,27 @@ class UrlPlugin
      * @var UrlFinder
      */
     private $urlFinder;
+    /**
+     * @var LandingPageContext
+     */
+    private $landingPageContext;
+    /**
+     * @var FilterManager
+     */
+    private $filterManager;
 
     /**
      * UrlPlugin constructor.
      * @param Resolver $layerResolver
      * @param UrlFinder $urlFinder
+     * @param LandingPageContext $landingPageContext
      */
-    public function __construct(Resolver $layerResolver, UrlFinder $urlFinder)
+    public function __construct(Resolver $layerResolver, UrlFinder $urlFinder, LandingPageContext $landingPageContext, FilterManager $filterManager)
     {
         $this->layerResolver = $layerResolver;
         $this->urlFinder = $urlFinder;
+        $this->landingPageContext = $landingPageContext;
+        $this->filterManager = $filterManager;
     }
 
     /**
@@ -44,13 +59,75 @@ class UrlPlugin
      */
     public function aroundGetSelectFilter(Url $subject, Closure $proceed, Item $filterItem)
     {
-        $layer = $this->layerResolver->get();
-        $filters = $layer->getState()->getFilters();
+        $layer = $this->getLayer();
+        $filters = $this->filterManager->getAllActiveFilters();
         $filters[] = $filterItem;
         if ($url = $this->urlFinder->findUrlByFilters($filters, $layer->getCurrentCategory()->getEntityId())) {
             return '/' . $url;
         }
 
         return $proceed($filterItem);
+    }
+
+    /**
+     * @param Url $subject
+     * @param Closure $proceed
+     * @param Item $filterItem
+     * @return mixed|string
+     * 
+     * When "hide_selected_filters" is disabled we need to apply some extra logic for the removal of filters.
+     * If the user removes a filter which is part of the landingPage predefined filters we need to go to the category page instead of the landingpage
+     */
+    public function aroundGetRemoveFilter(Url $subject, Closure $proceed, Item $filterItem)
+    {
+        $landingPage = $this->landingPageContext->getLandingPage();
+
+        $removeUrl = $proceed($filterItem);
+
+        // We are not on a landing page, no need to do anything special
+        if ($landingPage === null || $landingPage->getHideSelectedFilters()) {
+            return $removeUrl;
+        }
+
+        // The filter you want to remove is not set as predefined filter on the landingpage, we can safely stay on the landingpage
+        if (!$this->filterManager->isFilterAvailableOnLandingPage($landingPage, $filterItem)) {
+            return $removeUrl;
+        }
+
+        // Capture the filter part of the URL and rebuild the URL to from {landingPage}/{filters} to {category}/{filters}
+        if (preg_match('/' . $landingPage->getUrlPath() . '(.*)/', $removeUrl, $matches)) {
+            $category = $this->getLayer()->getCurrentCategory();
+            $categoryUrl = $category->getUrl();
+            $removeUrl = $categoryUrl . $matches[1];
+        }
+
+        return $removeUrl;
+    }
+
+    /**
+     * @param Url $subject
+     * @param Closure $proceed
+     * @param array $activeFilterItems
+     * @return mixed|string
+     */
+    public function aroundGetClearUrl(Url $subject, Closure $proceed, array $activeFilterItems)
+    {
+        $landingPage = $this->landingPageContext->getLandingPage();
+
+        // We are not on a landing page, no need to do anything special
+        if ($landingPage === null || $landingPage->getHideSelectedFilters()) {
+            return $proceed($activeFilterItems);
+        }
+
+        $category = $this->getLayer()->getCurrentCategory();
+        return $category->getUrl();
+    }
+
+    /**
+     * @return Layer
+     */
+    protected function getLayer(): Layer
+    {
+        return $this->layerResolver->get();
     }
 }
