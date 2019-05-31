35291c608c331a537695ef442f0296c61da2a53a
Edwin Jacobs <ejacobs@emico.nl>
2019-05-31 16:20:35 +0200

    Implemented slider url

--- src/Plugin/PathSlugStrategyPlugin.php
+++ src/Plugin/PathSlugStrategyPlugin.php
@@ -8,14 +8,11 @@ namespace Emico\AttributeLandingTweakwise\Plugin;
 
 
 use Closure;
-use Emico\AttributeLanding\Api\Data\LandingPageInterface;
-use Emico\AttributeLanding\Model\FilterHider\FilterHiderInterface;
 use Emico\AttributeLanding\Model\LandingPageContext;
 use Emico\AttributeLandingTweakwise\Model\FilterManager;
 use Emico\Tweakwise\Model\Catalog\Layer\Filter\Item;
 use Emico\Tweakwise\Model\Catalog\Layer\Url\Strategy\PathSlugStrategy;
-use Magento\Catalog\Model\Layer;
-use Magento\Catalog\Model\Layer\Resolver;
+use Emico\Tweakwise\Model\Catalog\Layer\UrlFactory;
 use Zend\Http\Request as HttpRequest;
 
 class PathSlugStrategyPlugin
@@ -31,13 +28,19 @@ class PathSlugStrategyPlugin
     private $filterManager;
 
     /**
+     * @var UrlFactory
+     */
+    private $urlFactory;
+
+    /**
      * @param LandingPageContext $landingPageContext
      * @param FilterManager $filterManager
      */
-    public function __construct(LandingPageContext $landingPageContext, FilterManager $filterManager)
+    public function __construct(LandingPageContext $landingPageContext, FilterManager $filterManager, UrlFactory $urlFactory)
     {
         $this->landingPageContext = $landingPageContext;
         $this->filterManager = $filterManager;
+        $this->urlFactory = $urlFactory;
     }
 
     /**
@@ -56,6 +59,34 @@ class PathSlugStrategyPlugin
 
         $filters = $this->filterManager->getActiveFiltersExcludingLandingPageFilters();
         $filters[] = $item;
+        return $pathSlugStrategy->buildFilterUrl($request, $filters);
+    }
+
+    /**
+     * @param PathSlugStrategy $pathSlugStrategy
+     * @param Closure $proceed
+     * @param HttpRequest $request
+     * @param Item $item
+     * @return string
+     */
+    public function aroundGetSliderUrl(PathSlugStrategy $pathSlugStrategy, Closure $proceed, HttpRequest $request, Item $item)
+    {
+        $landingPage = $this->landingPageContext->getLandingPage();
+        if ($landingPage === null || $landingPage->getHideSelectedFilters()) {
+            return $proceed($request, $item);
+        }
+
+        $filters = $this->filterManager->getActiveFiltersExcludingLandingPageFilters();
+        foreach ($filters as $key => $activeItem) {
+            if ($activeItem->getFilter()->getUrlKey() === $item->getFilter()->getUrlKey()) {
+                unset($filters[$key]);
+            }
+        }
+
+        $attribute = clone $item->getAttribute();
+        $attribute->setValue('title', '{{from}}-{{to}}');
+        $filters[] = new Item($item->getFilter(), $attribute, $this->urlFactory->create());
+
         return $pathSlugStrategy->buildFilterUrl($request, $filters);
     }
 
