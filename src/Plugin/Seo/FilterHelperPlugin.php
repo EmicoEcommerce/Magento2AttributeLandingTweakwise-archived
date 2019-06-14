84beb37ac44be7ff1607f71c5d3425ace1c1d421
Bram Gerritsen <bgerritsen@emico.nl>
2019-06-14 16:08:24 +0200

    When a filter is selected on landingpage, indexability of page should be checked

--- src/Plugin/Seo/FilterHelperPlugin.php
+++ src/Plugin/Seo/FilterHelperPlugin.php
@@ -7,6 +7,7 @@
 namespace Emico\AttributeLandingTweakwise\Plugin\Seo;
 
 use Emico\AttributeLanding\Model\LandingPageContext;
+use Emico\AttributeLandingTweakwise\Model\FilterManager;
 use Emico\Tweakwise\Model\Seo\FilterHelper;
 
 class FilterHelperPlugin
@@ -15,14 +16,20 @@ class FilterHelperPlugin
      * @var LandingPageContext
      */
     private $landingPageContext;
+    /**
+     * @var FilterManager
+     */
+    private $filterManager;
 
     /**
      * FilterHelperPlugin constructor.
      * @param LandingPageContext $landingPageContext
+     * @param FilterManager $filterManager
      */
-    public function __construct(LandingPageContext $landingPageContext)
+    public function __construct(LandingPageContext $landingPageContext, FilterManager $filterManager)
     {
         $this->landingPageContext = $landingPageContext;
+        $this->filterManager = $filterManager;
     }
 
     /**
@@ -32,7 +39,7 @@ class FilterHelperPlugin
      */
     public function aroundShouldPageBeIndexable(FilterHelper $helper, callable $proceed)
     {
-        if ($this->landingPageContext->isOnLandingPage()) {
+        if ($this->landingPageContext->isOnLandingPage() && \count($this->filterManager->getActiveFiltersExcludingLandingPageFilters()) === 0) {
             return true;
         }
         return $proceed();
